5cd3d31fea521bce04a35c62c5eb1b5ce3d3ce67
Eric Scott <eric.scott@acm.org>
2021-01-10 14:34:47 -0800

    Upgrading dependency versions, bumping devel branch version

--- project.clj
+++ project.clj
@@ -1,21 +1,21 @@
-(defproject ont-app/igraph-jena "0.1.0-SNAPSHOT"
+(defproject ont-app/igraph-jena "0.1.1-SNAPSHOT"
   :description "Library to port the Apache Jena APIs to the ont-app/iGraph protocol"
   :url "https://github.com/ont-app/igraph-jena"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [;; deps ambiguities
                  [com.cognitect/transit-clj "1.0.324"]
-                 [com.fasterxml.jackson.core/jackson-core "2.10.2"]
+                 [com.fasterxml.jackson.core/jackson-core "2.12.1"]
                  [org.slf4j/slf4j-api "1.7.30"]
-                 [org.clojure/tools.reader "1.3.2"]
+                 [org.clojure/tools.reader "1.3.4"]
                  ;; clojure
                  [org.clojure/clojure "1.10.1"]
-                 [org.clojure/spec.alpha "0.2.187"]
+                 [org.clojure/spec.alpha "0.2.194"]
                  ;; 3rd party libs
-                 [org.apache.jena/jena-core "3.14.0"]
-                 [org.apache.jena/jena-arq "3.14.0"]
-                 [org.apache.jena/jena-base "3.14.0"]
-                 [org.apache.jena/jena-iri "3.14.0"]
+                 [org.apache.jena/jena-core "3.17.0"]
+                 [org.apache.jena/jena-arq "3.17.0"]
+                 [org.apache.jena/jena-base "3.17.0"]
+                 [org.apache.jena/jena-iri "3.17.0"]
                  [org.slf4j/slf4j-simple "1.7.30"]
                  ;; Ont-app libs
                  [ont-app/graph-log "0.1.1"
@@ -34,7 +34,7 @@
                  [ont-app/vocabulary "0.1.3"
                   :exclusions [org.clojure/clojurescript
                                com.google.errorprone/error_prone_annotations]]
-                 [ont-app/rdf "0.1.1"
+                 [ont-app/rdf "0.1.2"
                   :exclusions [org.clojure/clojurescript
                                com.google.errorprone/error_prone_annotations
                                ]]
@@ -52,8 +52,7 @@
   :codox {:output-path "doc"}
 
   :profiles {:uberjar {}
-             :dev {:dependencies [[binaryage/devtools "0.9.10"]
-                                  ]
+             :dev {:dependencies []
                    :resource-paths  ["resources" "test/resources" ]
                    :source-paths ["src"]
                    :clean-targets
